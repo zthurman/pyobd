6a56acb7e8fa2447b2011d9095c3d0bedfb3c0c1
zthurman <zechariah.thurman@gmail.com>
2025-03-19 19:59:20 -0400

    Add tests for try_port
    I think the Python.h failure is
    because we don't call out the right
    python<ver>-devel in the package
    manager installs. Also needed to find
    real packages for fedora.

--- tests/unit/obd/utils/__init__.py
+++ tests/unit/obd/utils/__init__.py
@@ -10,12 +10,27 @@ class TestOBDStatus(TestCase):
 
     def test_NotConnected(self):
         self.assertEqual(self.status.NOT_CONNECTED, "Not Connected")
-    
+
     def test_ELMConnected(self):
         self.assertEqual(self.status.ELM_CONNECTED, "ELM Connected")
-    
+
     def test_OBDConnected(self):
         self.assertEqual(self.status.OBD_CONNECTED, "OBD Connected")
-    
+
     def test_CarConnected(self):
         self.assertEqual(self.status.CAR_CONNECTED, "Car Connected")
+
+
+class MockSerialObjectWithClose:
+    def close(self):
+        pass
+
+
+class TestTryPort(TestCase):
+    def test_try_port_fail(self):
+        self.assertEqual(utils.try_port("testport"), False)
+
+    def test_try_port_success(self):
+        with patch("serial.Serial") as mocked_port:
+            mocked_port.return_value = MockSerialObjectWithClose()
+            self.assertEqual(utils.try_port("testport"), True)
